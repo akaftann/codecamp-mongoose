03df21f9ba1c4f71854c100b1d205a424a345d07
mac <mac@MacBook-Pro-mac.local>
2023-04-23 13:05:56 +0300

    added connection string

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+const mongoose = require("mongoose");
+const dbUrl = process.env.MONGO_URI
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
